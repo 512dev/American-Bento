b3364703b991fa8f7f23f37dbd79f369535cd03a
Owen Klumb <owen.klumb@gmail.com>
2017-04-27 14:49:47 +0900

    Magnific-popup
    Attempting to add popup to order button

--- app.js
+++ app.js
@@ -11,5 +11,25 @@ $(".tabs-item").hover(function() {
 	$(this).removeClass('active');
 });
 
+$('.button').click(function(event) {
+	$('.popup-with-form').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#name',
+
+		// When elemened is focused, some mobile browsers in some cases zoom in
+		// It looks not nice, so we disable it:
+		callbacks: {
+			beforeOpen: function() {
+				if($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#name';
+				}
+			}
+		}
+	});
+});
+
  
 });
